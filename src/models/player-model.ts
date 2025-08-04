1eb48a266356e13167e0b17e5e6feb411301d145
Cristian <cristian.santos0308@gmail.com>
2025-08-04 16:31:49 -0300

    Refactored PlayerModel

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,4 +1,18 @@
 export interface PlayerModel {
     id: number,
-    name: string
+    name: string,
+    club: string,
+    nationality: string,
+    position: string,
+    statistics: Statistics
+}
+
+interface Statistics {
+    Overall: number,
+    Pace: number,
+    Shooting: number,
+    Passing: number,
+    Dribbling: number,
+    Defending: number,
+    Physical: number
 }
