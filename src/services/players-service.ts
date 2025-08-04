879a245a07c56c4bc28051635ca18e64fb43e482
Cristian <cristian.santos0308@gmail.com>
2025-08-04 16:15:04 -0300

    Created repository layer

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,4 +1,15 @@
+import { findAllPlayers } from "../repositories/players-repository";
+import { noContent, ok } from "../utils/http-helpers";
 
 export const getPlayerService = async () => {
-    return {  player: "Messi"};
+    const data = await findAllPlayers();
+    let response = null;
+
+    if (data) {
+        response = await ok(data);
+    } else {
+        response = await noContent();
+    }
+
+    return response;
 }
